82d216e7427874422b907c7c5b562bb30dedfc36
Adilson Medeiros <adilsonmedeiros70@gmail.com>
2023-10-17 07:52:28 -0300

    Arquivo atualizado

--- projetoInterdisciplinar/src/main/java/module-info.java
+++
@@ -1,7 +0,0 @@
-module projeto.projetointerdisciplinar {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-    opens projeto.projetointerdisciplinar to javafx.fxml;
-    exports projeto.projetointerdisciplinar;
-}
