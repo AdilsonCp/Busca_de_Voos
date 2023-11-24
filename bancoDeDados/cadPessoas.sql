13c4ee10f1f83f1b414f7917105778fe50bf24e1
Adilson Medeiros <adilsonmedeiros70@gmail.com>
2023-11-24 09:37:18 -0300

    Alterando banco

---
+++ bancoDeDados/cadPessoas.sql
@@ -0,0 +1,34 @@
+insert into pessoas(nome, cpf, genero, data_nascimento) values ('Eduardo', '86229821060', 'Masculino',"2003-02-16 00:00:00");
+insert into endereco(pessoa_id, cep, endereco, numero, complemento, bairro, cidade, uf )
+	values(1, '13199635','Avenida Senador Saraiva','32',null,'Cambui','Campinas','São Paulo');
+insert into credenciais(pessoa_id, email, senha) values (1,'eduardo.edu@gmail.com','Eduardo1234');
+insert into contato(pessoa_id, telefone, email) values (1,'19987457892','eduardo.edu@gmail.com');
+
+
+insert into pessoas(nome, cpf, genero, data_nascimento) values ('Maria', '58769720029', 'Feminino',"2004-12-16 00:00:00");
+insert into endereco(pessoa_id, cep, endereco, numero, complemento, bairro, cidade, uf ) 
+	values(2, '13192635','Avenida Brasil','76',null,'Jardim Amanda','Hortolândia','São Paulo');
+insert into credenciais(pessoa_id, email, senha) values (2,'maria@hotmail.com','Horto123');
+insert into contato(pessoa_id, telefone, email) values (2,'19984558202','maria@hotmail.com');
+
+
+insert into pessoas(nome, cpf, genero, data_nascimento) values ('Eder', '20758804091', 'Masculino',"
+2013-02-16 00:00:00");
+insert into endereco(pessoa_id, cep, endereco, numero, complemento, bairro, cidade, uf ) 
+	values(3, '13197798','Rua Almeida','875','Bloco A','Oreste Ongaro','Hortolândia','São Paulo');
+insert into credenciais(pessoa_id, email, senha) values (3,'eder.araujo@gmail.com','araujo@13');
+insert into contato(pessoa_id, telefone, email) values (3,'19984557788','eder.araujo@gmail.com');
+
+insert into pessoas(nome, cpf, genero, data_nascimento) values ('Gabriel Mesquita', '66633322212', 'Masculino',"1998-1-10 00:00:00");
+insert into endereco(pessoa_id, cep, endereco, numero, complemento, bairro, cidade, uf ) 
+	values(4, '1318832','Av. John Boyd Dunlop','76',null,'Jardim Garcia','Campinas','São Paulo');
+insert into credenciais(pessoa_id, email, senha) values (4,'gabriel@hotmail.com','Gabriel1234');
+insert into contato(pessoa_id, telefone, email) values (4,'19983648987','gabriel@hotmail.com');
+
+
+insert into pessoas(nome, cpf, genero, data_nascimento) values ('Rebecca', '66677798820', 'Feminino',"
+1990-02-16 00:00:00");
+insert into endereco(pessoa_id, cep, endereco, numero, complemento, bairro, cidade, uf ) 
+	values(5, '1318896','Rua Almeida','875','Bloco A','Oreste Ongaro','Hortolândia','São Paulo');
+insert into credenciais(pessoa_id, email, senha) values (5,'rebecca@gmail.com','rebeca@13');
+insert into contato(pessoa_id, telefone, email) values (5,'19984555252','rebecca@gmail.com');
